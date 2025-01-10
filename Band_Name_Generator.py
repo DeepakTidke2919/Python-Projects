722405827f7c112787c87f8c53609a15f5fe3d31
DeepakTidke2919 <deeptidke2919@gmail.com>
2025-01-10 23:44:48 +0530

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,12 @@
+print("***** Welcome to the Band Name Generator *****")
+
+your_name = input("What is your name?")
+
+print(your_name)
+
+City_name = input("Which city you were born?")
+
+print(City_name)
+
+print("Your band could be " + your_name + " " + City_name)
+
