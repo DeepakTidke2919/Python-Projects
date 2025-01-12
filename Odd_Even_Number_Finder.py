02c09f368781a2c6650fcb3c65ef77003c8b8fb8
deep <deeptidke2919@gmail.com>
2025-01-12 19:18:21 +0530

    Odd_Even_Number

---
+++ Odd_Even_Number_Finder.py
@@ -0,0 +1,13 @@
+print("****Welcome to Odd or Even Number finder program **** ")
+
+Given_Number = int(input("Please enter the Number as you desire "))
+
+print(Given_Number)
+
+#print(101 % 2)
+
+if Given_Number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
