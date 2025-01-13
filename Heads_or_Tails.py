6672ea33f55123af61caef5338f52b45ced5bcc4
deep <deeptidke2919@gmail.com>
2025-01-13 20:19:48 +0530

    Heads_or_Tails

---
+++ Heads_or_Tails.py
@@ -0,0 +1,16 @@
+import random
+
+
+print("Head or Tails coin toss")
+
+number = random.randint(a=1 , b=100)
+
+print(number)
+
+if number % 2 == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+
+
